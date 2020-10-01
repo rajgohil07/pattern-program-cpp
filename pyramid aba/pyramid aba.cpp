f4e37772f8390c2e7733f5f71fb0f69785e4149b
Raj Gohil <71960874+rajgohil07@users.noreply.github.com>
2020-10-01 14:16:14 +0530

    Create pyramid aba.cpp

---
+++ pyramid aba/pyramid aba.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+    char r;
+    cout<<"Enter the number of rows.......\n\n";
+    cin>>k;
+    for(i=1;i<=k;i++)
+    {
+        r='A';
+    for(j=1;j<=2*k-1;j++)
+    {
+        if(j>=(k+1)-i && j<=(k-1)+i)
+            {
+                cout<<r;
+                if(k>j)
+                    r++;
+                else
+                    r--;
+            }
+        else
+            cout<<" ";
+        }
+         cout<<endl;
+    }
+}
